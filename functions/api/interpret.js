5869248bb1ddd1a7b06f3c4baca8afac5545a5a6
Yindin <yindin777@gmail.com>
2024-12-17 12:53:43 +0100

    Update interpret.js

--- functions/api/interpret.js
+++ functions/api/interpret.js
@@ -1,22 +1,31 @@
-// functions/api/interpret.js
-
 export async function onRequestPost(context) {
     try {
         const geminiApiKey = context.env.GEMINI_API_KEY; // Accessing the secret API key from environment variable, replace with your env name if different
         const requestBody = await context.request.json();
         const keyword = requestBody.keyword;
         const interpretationStyle = requestBody.interpretationStyle;
+         const language = requestBody.language;
+
 
 
       if (!geminiApiKey) {
         return new Response('GEMINI_API_KEY is not set in environment variables.', { status: 500 });
       }
+      let prompt = ''
 
-        const prompt = `Interpret the dream keyword "${keyword}" from a ${interpretationStyle} perspective. Provide interpretations based on Judeo-Christian and Islamic literature, including references. Also, explain the possible influence of lifestyle factors.`
+      if (language === 'tr'){
+          prompt = `İbn-i Sirin'in kitabından ve diğer İslami kaynaklardan yararlanarak, "${keyword}" rüya kelimesini ${interpretationStyle} perspektifinden yorumlayın. Ayrıca, rüyanın olası yaşam tarzı, biyolojik ve yaş faktörleri üzerindeki etkisini açıklayın. Ayrıca Yahudi-Hristiyan kaynaklarına ve modern bilimsel kaynaklara da başvurun.`
+      } else if (language === 'en') {
+        prompt = `Interpret the dream keyword "${keyword}" from a ${interpretationStyle} perspective, incorporating insights from İbn-i Sirin's book and other Islamic literature. Also, explain the possible influence of lifestyle, biological, and age factors on the dream, and include insights from Judeo-Christian texts and modern scientific perspectives.`
+      } else if (language === 'fr') {
+         prompt = `Interprétez le mot-clé de rêve "${keyword}" dans une perspective ${interpretationStyle}, en intégrant des éléments du livre d'İbn-i Sirin et d'autres textes islamiques. Expliquez aussi l'influence possible du style de vie, des facteurs biologiques et de l'âge sur le rêve, tout en incluant des perspectives des textes judéo-chrétiens et des approches scientifiques modernes.`
+      } else if (language === 'ar') {
+           prompt = `فسر الكلمة المفتاحية للحلم "${keyword}" من منظور ${interpretationStyle}، مع دمج رؤى من كتاب ابن سيرين والأدب الإسلامي الآخر. اشرح أيضًا التأثير المحتمل لنمط الحياة والعوامل البيولوجية والعمر على الحلم، وقم بتضمين رؤى من النصوص اليهودية المسيحية ووجهات النظر العلمية الحديثة.`
+       }
+
 
 
         const geminiAPIUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + geminiApiKey;
-
 
         const payload = {
            contents: [
